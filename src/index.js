d31d2909b6012e28a02d78976142d6a788a3be82
jechudc2001 <lu2001dc@gmail.com>
2024-12-02 02:44:16 -0500

    fix sessions 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path"; // Para manejar rutas de archivos
 import { fileURLToPath } from "url";
-import session from "express-session";
+import session from 'cookie-session';
 
 
 // Importar rutas
@@ -35,6 +35,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Middleware para analizar cuerpos JSON
 app.use(express.json());
+
+app.set("trust proxy", 1);
 
 
 
